0b67ce3408fce5918c996a7c803b6c8aca880d50
Bojan Markovski <markovski.bojan@hotmail.com>
2019-04-23 16:41:50 +0200

    Ajax in Our services

--- wp-content/themes/itcrowd_theme/js/OurServices.js
+++ wp-content/themes/itcrowd_theme/js/OurServices.js
@@ -17,3 +17,58 @@ var onloadCallback = function() {
     'sitekey': '6LdZIpcUAAAAAI2bvnnVOLYrvDDS6Wu1pHefdfv6'
   });
 };
+
+// AJAX
+
+$(document).ready(function() {
+  $("form").submit(function(e) {
+    e.preventDefault(); // Prevent a new window from opening upon clicking 'Subscribe now' button
+
+    var validForm = true; // Set initial state of valid form to true
+    var inputArray = $(this).find("input.required"); // Find all required inputs and store them in array
+
+    // Simple check for all inputs to make sure the value is not empty
+    inputArray.each(function(item) {
+      if ($(this).val() == "") {
+        validForm = false;
+        console.log("Empty inputs")
+      }
+    });
+
+    // Everything checks out! Continue...
+    if (validForm == true) {
+      // var formContainer = $("#exampleModal");
+      var formData = $(this).serializeArray(); // Format all info and get it ready for sendoff
+
+      // AJAX magic coming up...
+      $.ajax({
+        type: $(this).attr("method"),
+        url: $(this).attr("action"),
+        data: formData,
+        cache: false,
+        contentType: "application/x-www-form-urlencoded",
+        encode: true,
+        error: function(err) {
+          console.log("Uh, oh. There was an error:", err); // You broke something...
+        },
+        success: function(data) {
+          if(data.status == "validation_failed"){
+            alert("validation_failed");
+          } else {
+            console.log("Success! Here is the data:", data); // Yay!
+
+            // $(formContainer).hide(); // Hide the initial form
+            $("body").removeClass("modal-open");
+            $(".modal.fade").removeClass("in").css("display", "none");
+            $(".modal").removeClass("in");
+            $(".modal").removeClass("fade");
+            // $(".modal").removeClass("in");
+
+          }
+        }
+      });
+    }
+
+    return; // No go on form...
+  }); // end of submit function
+});
--- wp-content/themes/itcrowd_theme/services.php
+++ wp-content/themes/itcrowd_theme/services.php
@@ -170,7 +170,7 @@
                   <h2>Contact Us</h2>
                </div>
                <div class="modal-body">
-                <form action="/wp-json/contact-form-7/v1/contact-forms/227/feedback" method="POST" id="hire-us-form">
+                <form action="/wordpress/wp-json/contact-form-7/v1/contact-forms/227/feedback" method="POST" id="hire-us-form">
                      <div class="form-group">
                         <!-- NAME INPUT -->
                         <div class="holder">
@@ -212,14 +212,13 @@
                     </div>
                     <div class="modal-footer">
                         <button id="btn-close" type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-secondary">Send e-mail</button>
+                        <button type="submit" class="btn btn-secondary btn-submit">Send e-mail</button>
                     </div>
                 </form>
             </div>
          </div>
       </div>
       
-    <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d741.1834924104721!2d21.391826829250995!3d42.00598459524209!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x135415ca41a4d55b%3A0x7008af860fc64af5!2sITCrowd!5e0!3m2!1sen!2smk!4v1555326107615!5m2!1sen!2smk" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe> -->
 <!-- Content END -->
 
 <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/OurServices.js"></script>
